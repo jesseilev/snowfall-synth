551ba457c41dd9afa104b1ec10d2bfe8e56ab01a
jesseilev <jesseilev@gmail.com>
2019-03-13 01:50:41 -0400

    Updates

---
+++ precache-manifest.aa34fd7c4e5aeae888f61119c08aea9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/snowfall-synth/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4b9ffa6e2c0263098298",
+    "url": "/snowfall-synth/static/js/runtime~main.7cffbfd4.js"
+  },
+  {
+    "revision": "6f33b63dad6eb0d7119f",
+    "url": "/snowfall-synth/static/js/main.3f962ca7.chunk.js"
+  },
+  {
+    "revision": "5a07805274a5b36bc628",
+    "url": "/snowfall-synth/static/js/2.bd17c0ad.chunk.js"
+  },
+  {
+    "revision": "6f33b63dad6eb0d7119f",
+    "url": "/snowfall-synth/static/css/main.4fb9c9b2.chunk.css"
+  },
+  {
+    "revision": "366f883773f0b9a9ae8daa2bcc055407",
+    "url": "/snowfall-synth/index.html"
+  }
+];
